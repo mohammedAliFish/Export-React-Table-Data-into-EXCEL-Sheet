cd5dd2808e696e6029010ad9fe17b5bd74d7e8a8
mohammedH97 <102896991+mohammedH97@users.noreply.github.com>
2024-04-05 16:45:42 +0300

    Export React Table Data into EXCEL Sheet

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+
+import './App.css'
+import {useState,useEffect} from 'react';
+import ReactHTMLTableToExcel from 'react-html-table-to-excel';
+
+function App() {
+
+  const[result,setResult]= useState([]);
+ 
+  const getData = ()=>
+  {
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(res => setResult( res));
+  }
+  
+  useEffect(() => {
+      getData();
+  },)
+  return (
+    <div className="container">
+    <h3 className="mt-3 text-success"><center>Export React Table Data into EXCEL Sheet</center></h3>
+    <div className="row mt-4">
+    <ReactHTMLTableToExcel
+        id="test-table-xls-button"
+        className="download-table-xls-button btn btn-success mb-3"
+        table="table-to-xls"
+        filename="tablexls"
+        sheet="tablexls"
+        buttonText="Export Data to Excel Sheet"/>
+       <table className="table" id="table-to-xls" border={1}>
+        <thead className="thead-dark">
+        <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Email</th>
+            <th>Website</th>
+        </tr>
+        </thead>
+        <tbody>
+       
+             {result.map((res)=>
+                <tr>
+                <td>{res.name}</td>
+                <td>{res.username}</td>
+                <td>{res.email}</td>
+                <td>{res.website}</td>
+                </tr>
+              )}   
+           
+        </tbody>   
+    </table>
+ </div>
+</div>
+  )
+}
+
+export default App
